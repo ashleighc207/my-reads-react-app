1a7448fd2a306f65b64d2be17fc0e63bdf76b38a
Ashleigh <ashleighc207@gmail.com>
2018-07-17 20:46:25 +0000

    Add Search Feature for Books

--- src/App.js
+++ src/App.js
@@ -7,20 +7,28 @@ import { Route } from 'react-router-dom'
 
 class BooksApp extends Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
   }
-
+  
+  componentDidMount(){
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
+  }
+  
   render() {
     return (
       <div className="app">
-      <Route exact path="/search" component={Search}/>
-          <BookShelf/>
+        <Route exact path='/' render={() => (
+          <BookShelf
+            books={this.state.books}
+          />
+        )}/>
+        <Route exact path='/search' render={() => (
+          <Search
+            books={this.state.books}
+          />
+        )}/>
       </div>
     )
   }
--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
 
 class Search extends Component {
+    state = {
+        query: ''
+    }
+    
+    updateQuery = (query) => {
+        this.setState({ query })
+    }
+    
+    clearQuery = () => {
+        this.setState({query: ''})
+    }
+    
     render() {
+        const { query } = this.state
+        const { books } = this.props
+        let searchBooks;
+        if (query){
+            const match = new RegExp(escapeRegExp(query), 'i')
+            searchBooks = books.filter((book) => match.test(book.title) || match.test(book.authors))
+        } else {
+            searchBooks = books;
+        }
+        searchBooks.sort(sortBy('title'))
+        
         return (
             <div className="search-books">
             <div className="search-books-bar">
@@ -19,12 +41,38 @@ class Search extends Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+                <input 
+                type="text" 
+                placeholder="Search by title or author" 
+                value={query}
+                onChange={(event) => this.updateQuery(event.target.value)}
+                />
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+             <ol className="books-grid">
+            {searchBooks.map((book) => (
+                        <li key={book.id} className='books-grid'>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: "url(" + `${book.imageLinks.smallThumbnail}` + ")" }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors}</div>
+                        </div>
+                        </li>
+                    ))}
+             </ol>
             </div>
           </div> 
         )
