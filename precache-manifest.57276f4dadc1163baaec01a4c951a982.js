33926283884503d023918dd0c69f430ed19ea9e5
Ashleigh <ashleighc207@gmail.com>
2020-08-09 00:22:31 -0400

    Updates

---
+++ precache-manifest.57276f4dadc1163baaec01a4c951a982.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9686b3e384e4a2d1660faf739aed22d7",
+    "url": "/my-reads-react-app/index.html"
+  },
+  {
+    "revision": "bea10c748e9b6746448e",
+    "url": "/my-reads-react-app/static/css/main.adef2db0.chunk.css"
+  },
+  {
+    "revision": "8428a46a4df011358e50",
+    "url": "/my-reads-react-app/static/js/2.46ba43b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-reads-react-app/static/js/2.46ba43b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bea10c748e9b6746448e",
+    "url": "/my-reads-react-app/static/js/main.16b8fea7.chunk.js"
+  },
+  {
+    "revision": "3489676ffc798c03e316",
+    "url": "/my-reads-react-app/static/js/runtime-main.3b54b2bc.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/my-reads-react-app/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/my-reads-react-app/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-reads-react-app/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
